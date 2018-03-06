6bb6104d33dfecd7b82b0aa828054d61afba682e
Cory Sales <cory.sales@okstate.edu>
2018-03-06 14:30:53 -0600

    commit

--- SquatterForm/SquatterForm/Default.aspx.designer.cs
+++ SquatterForm/SquatterForm/Default.aspx.designer.cs
@@ -13,6 +13,51 @@ namespace SquatterForm {
     public partial class _Default {
         
         /// <summary>
+        /// lblZipCode control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListBox lblZipCode;
+        
+        /// <summary>
+        /// ddlTotalAmountDue control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlTotalAmountDue;
+        
+        /// <summary>
+        /// ddlPaymentAgreement control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlPaymentAgreement;
+        
+        /// <summary>
+        /// ddlGoodArea control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlGoodArea;
+        
+        /// <summary>
+        /// ddlBadArea control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlBadArea;
+        
+        /// <summary>
         /// grdDTPData control.
         /// </summary>
         /// <remarks>
