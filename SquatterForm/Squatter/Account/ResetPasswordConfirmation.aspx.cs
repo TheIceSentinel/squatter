2d97313dca4ae957126df226a56843774f0b77ee
TheIceSentinel <james.c.doty@gmail.com>
2018-04-15 14:39:57 -0500

    Transfer to solution with integrated security. Packages upgraded.

---
+++ SquatterForm/Squatter/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace Squatter.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ SquatterForm/Squatter/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Squatter.Global" Language="C#" %>
